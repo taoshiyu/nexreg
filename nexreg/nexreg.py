afce716522c911678ed7d1ce2adbe7b3a6ecb7a0
taoshiyu <sss1063750852@126.com>
2019-08-19 11:01:01 +0800

    '2019.8.19'

--- nexreg/nexreg.py
+++ nexreg/nexreg.py
@@ -1,13 +1,15 @@
 # coding=utf-8
 
 import datetime
+import logging
 import time
 from collections import deque
 
-import matplotlib.pyplot as plt
 import pymongo
 import requests
 from lxml import etree
+
+import settings
 
 url = 'https://apa.nexregreporting.com/Home/GetRequestedData?isin=&mic=&type=pre&currentDay=true&region=UK&pageIndex=1&pageSize=100'
 url_ = 'https://apa.nexregreporting.com/Home/GetRequestedData?isin=&mic=&type=pre&currentDay=true&region=UK&pageIndex={}&pageSize={}'
@@ -27,18 +29,27 @@ thead = ['instrumentCode', 'quantity', 'searchTime', 'priceLevel', 'bidOfferStat
 mongo_client = pymongo.MongoClient(host='localhost', port=27017)
 db = mongo_client['tradeWeb']
 
-q1 = deque(maxlen=100)
-q2 = deque(maxlen=100)
-plt.ion()
-
 start = datetime.datetime.now()
 time_delta = datetime.timedelta(hours=6)
 end = start + time_delta
 
 now_point = 0
 
+logger = logging.getLogger('nexreg.py')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(settings.LOG_FILE)
+formatter = logging.Formatter('[%(asctime)s]-%(name)s-%(levelname)s:%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
+
+# real-time display update-count while crawling
 def search_and_plot():
+    import matplotlib.pyplot as plt
+    q1 = deque(maxlen=100)
+    q2 = deque(maxlen=100)
+    plt.ion()
+    logger.info('function search_and_plot running')
     plt.figure(1)
     global now_point
     with requests.session() as s:
@@ -51,14 +62,10 @@ def search_and_plot():
                 r = s.get(url)
                 r.raise_for_status()
             except Exception as e:
-                print(e)
+                logger.exception(e)
                 continue
-            print(r.status_code)
             tree = etree.HTML(r.text)
             trs = tree.xpath('//table[@id="apa-data"]/tbody/tr')
-            # thd = tree.xpath('//table[@id="apa-data"]/thead/tr/th/text()')
-            # thd = list(map((lambda x:x.strip()),thd))
-            # assert thd == thead
             update_count = 0
             for tr in trs:
                 td = tr.xpath('td/text()')
@@ -67,28 +74,26 @@ def search_and_plot():
                 result = db['nexreg'].update_one(filter=doc, update={'$set': doc}, upsert=True)  # r.raise_for_status()
                 if not result.matched_count:
                     update_count += 1
+            logger.info('update %s records' % update_count)
             q2.append(update_count)
             plt.plot(q1.copy(), q2.copy(), '-r')
-            plt.pause(0.1)
+            plt.pause(settings.DOWNLOAD_DELAY)
             now_ = datetime.datetime.now()
-        plt.savefig('figure-1.png')
 
 
+# crawler
 def search():
+    logging.info('function search running')
     global now_point
     with requests.session() as s:
         s.headers = headers
-        now_ = datetime.datetime.now()
-        while now_ < end:
-            now_str = now_.strftime('%m:%S')
-            q1.append(now_str)
+        while True:
             try:
                 r = s.get(url)
                 r.raise_for_status()
             except Exception as e:
-                print(e)
+                logger.exception(e)
                 continue
-            print(r.status_code)
             tree = etree.HTML(r.text)
             trs = tree.xpath('//table[@id="apa-data"]/tbody/tr')
             update_count = 0
@@ -99,12 +104,13 @@ def search():
                 result = db['nexreg'].update_one(filter=doc, update={'$set': doc}, upsert=True)  # r.raise_for_status()
                 if not result.matched_count:
                     update_count += 1
-            time.sleep(1)
-            now_ = datetime.datetime.now()
-        plt.savefig('figure-1.png')
+            logging.info('update %s documents' % update_count)
+            time.sleep(settings.DOWNLOAD_DELAY)
 
 
+# crawl history data
 def search_all():
+    logger.info('\n\nfunction search_all running')
     page_index = 1
     with requests.session() as s:
         s.headers = headers
@@ -123,16 +129,13 @@ def search_all():
                 result = db['nexreg'].update_one(filter=doc, update={'$set': doc}, upsert=True)
                 if not result.matched_count:
                     update_count += 1
-            print(r.status_code, update_count)
+                logger.info('update %s records')
+            time.sleep(settings.DOWNLOAD_DELAY)
             page_index += 1
 
 
-def plot_():
-    l1 = [1, 2, 1]
-    l2 = [1, 2, 1]
-    plt.plot(l1, l2, '-r')
-    plt.pause(5)
-
-
 if __name__ == '__main__':
-    search_all()
+    print('running ...')
+    # search()
+    search_and_plot()
+    # search_all()
---
+++ nexreg/settings.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+MONGO_HOST = 'localhost'
+MONGO_DB = ''
+MONGO_COLLECTION = ''
+
+LOG_FILE = 'log.log'
+
+DOWNLOAD_DELAY = '1'
+
